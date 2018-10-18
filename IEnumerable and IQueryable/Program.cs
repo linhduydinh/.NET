6e8faa70994491f8129d4b9becf169da3508a39d
Tyler Dinh <linh.dinh@orientsoftware.net>
2018-10-18 15:06:04 +0700

    data reader from database

---
+++ IEnumerable and IQueryable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_and_IQueryable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=LINHDD-LAPTOP;Initial Catalog=northwind;Integrated Security=True";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var query = "Select * from Categories";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader =  cmd.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine("{0}\t{1}", reader.GetInt32(0), reader.GetString(1));
+                    }
+                }
+            }
+        }
+    }
+}
